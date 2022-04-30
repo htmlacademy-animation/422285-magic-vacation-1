dd71de745a830cbc449aed91f806059544ea77f2
pochel10 <alexeyzharkov@@gmail.com>
2022-04-30 12:10:09 +0300

    module2-task2

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import setTypographyAnimation from './modules/accent-typography-build.js';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+setTypographyAnimation();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
